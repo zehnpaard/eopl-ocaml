26f06496d94fc4ac4ff189bc717b8fff162ebff7
zehnpaard <zehnpaard@googlemail.com>
2018-11-07 18:53:23 +0900

    Update letrec to cps

--- ch05/exception.ml
+++ ch05/exception.ml
@@ -24,6 +24,17 @@ and procedure =
   | Procedure of symbol * expression * environment
 ;;
 
+type continuation =
+  | EndCont
+  | ZeroCont of continuation
+  | LetCont of symbol * expression * environment * continuation
+  | IfCont of expression * expression * environment * continuation
+  | Diff1Cont of expression * environment * continuation
+  | Diff2Cont of expVal * continuation
+  | CallFuncCont of expression * environment * continuation
+  | CallArgCont of expVal * continuation
+;;
+
 type program = Program of expression;;
 
 exception CannotConvertNonNumVal;;
@@ -56,37 +67,51 @@ let rec applyEnv env var = match env with
           else applyEnv env1 var
 ;;
 
-
-let rec valueOf exp env = match exp with
+let rec valueOf exp env cont = match exp with
   | ConstExp n ->
-          NumVal n
+          applyCont cont (NumVal n)
   | DiffExp (e1, e2) ->
-          let n1 = expValToNum (valueOf e1 env) in
-          let n2 = expValToNum (valueOf e2 env) in
-          NumVal (n1 - n2)
+          valueOf e1 env (Diff1Cont (e2, env, cont))
   | ZeroExp e ->
-          BoolVal (0 = expValToNum (valueOf e env))
+          valueOf e env (ZeroCont cont)
   | IfExp (e1, e2, e3) ->
-          if expValToBool (valueOf e1 env)
-          then valueOf e2 env
-          else valueOf e3 env
+          valueOf e1 env (IfCont (e2, e3, env, cont))
   | VarExp var ->
-          applyEnv env var
+          applyCont cont (applyEnv env var)
   | LetExp (var, e, body) ->
-          valueOf body (ExtendEnv (var, (valueOf e env), env))
+          valueOf e env (LetCont (var, body, env, cont))
   | ProcExp (var, body) ->
-          ProcVal (Procedure (var, body, env))
+          applyCont cont (ProcVal (Procedure (var, body, env)))
   | CallExp (func, arg) ->
-          let p = expValToProc (valueOf func env) in
-          applyProcedure p (valueOf arg env)
+          valueOf func env (CallFuncCont (arg, env, cont))
   | LetRecExp (fname, farg, fbody, body) ->
-          valueOf body (ExtendEnvRec (fname, farg, fbody, env))
-and applyProcedure p v = match p with
-  | Procedure (var, body, senv) -> valueOf body (ExtendEnv (var, v, senv))
+          valueOf body (ExtendEnvRec (fname, farg, fbody, env)) cont
+and applyProcedure p v cont = match p with
+  | Procedure (var, body, senv) ->
+          valueOf body (ExtendEnv (var, v, senv)) cont
+and applyCont cont val1 = match cont with
+  | EndCont -> val1
+  | ZeroCont sc ->
+          let val2 = BoolVal (0 = expValToNum val1) in
+          applyCont sc val2
+  | LetCont (var1, body, env, sc) ->
+          valueOf body (ExtendEnv (var1, val1, env)) sc
+  | IfCont (e2, e3, env, sc) ->
+          if expValToBool val1
+          then valueOf e2 env sc
+          else valueOf e3 env sc
+  | Diff1Cont (e2, env, sc) ->
+          valueOf e2 env (Diff2Cont (val1, sc))
+  | Diff2Cont (v1, sc) ->
+          applyCont sc (NumVal (expValToNum v1 - expValToNum val1))
+  | CallFuncCont (arg, env, sc) ->
+          valueOf arg env (CallArgCont (val1, sc))
+  | CallArgCont (v1, sc) ->
+          applyProcedure (expValToProc v1) val1 sc
 ;;
 
 let valueOfProgram = function
-  | Program e -> valueOf e EmptyEnv
+  | Program e -> valueOf e EmptyEnv EndCont
 ;;
 
 
