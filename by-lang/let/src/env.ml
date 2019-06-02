a6ba32b73dada780b7316aba57564bdf1c01f608
zehnpaard <zehnpaard@googlemail.com>
2019-06-03 04:33:15 +0900

    Add environment type

---
+++ by-lang/let/src/env.ml
@@ -0,0 +1,9 @@
+type t = (string * Val.t) list
+
+let empty = []
+
+let rec find env s = match env with
+  | [] -> None
+  | (s', v')::env' -> if s = s' then v' else find env' s
+
+let extend env s v = (s, v)::env
