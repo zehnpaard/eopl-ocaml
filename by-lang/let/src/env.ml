b88538f49b56e8dcc1d1933e32cbb8e08670e451
zehnpaard <zehnpaard@googlemail.com>
2019-06-03 04:39:23 +0900

    Add Some to return value of find

--- by-lang/let/src/env.ml
+++ by-lang/let/src/env.ml
@@ -4,6 +4,6 @@ let empty = []
 
 let rec find env s = match env with
   | [] -> None
-  | (s', v')::env' -> if s = s' then v' else find env' s
+  | (s', v')::env' -> if s = s' then Some v' else find env' s
 
 let extend env s v = (s, v)::env
