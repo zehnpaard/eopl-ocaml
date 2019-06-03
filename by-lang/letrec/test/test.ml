d87a70036eca095d12a7eb704ffc930eaf9833e8
zehnpaard <zehnpaard@googlemail.com>
2019-06-03 12:44:36 +0900

    Add test for letrec

--- by-lang/letrec/test/test.ml
+++ by-lang/letrec/test/test.ml
@@ -16,3 +16,4 @@ let _ =
   rep_str "let f = proc (x) -(x, 11) in (f (f 77))";
   rep_str "(proc (f) (f (f 77)) proc (x) -(x, 11))";
   rep_str "let x = 200 in let f = proc (z) -(z, x) in let x = 100 in let g  = proc (z) -(z, x) in -((f 1), (g 1))";
+  rep_str "letrec zero (x) = if zero?(x) then x else (zero -(x, 1)) in (zero 5)";
