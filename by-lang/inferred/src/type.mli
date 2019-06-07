102f7e32736d675558e18d02dc319921b429062f
zehnpaard <zehnpaard@googlemail.com>
2019-06-07 21:14:13 +0900

    Add Type.Var and related functions

--- by-lang/inferred/src/type.mli
+++ by-lang/inferred/src/type.mli
@@ -2,3 +2,9 @@ type t = Int
        | Bool
        | Proc of t * t
        | Unknown
+       | Var of int
+
+val init : unit -> unit
+val free : unit -> t
+val make_concrete : t -> t
+val no_occurrence : t -> t -> bool
