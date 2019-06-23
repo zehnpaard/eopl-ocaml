fe0201703817bb3dcf87b1e81251d65e4f5dd1cd
zehnpaard <zehnpaard@googlemail.com>
2019-06-23 10:49:34 +0900

    Add store

---
+++ by-lang/explicit-refs/src/store.ml
@@ -0,0 +1,24 @@
+type t = Val.t list ref
+
+let v = (ref [] : t)
+let n = ref 0
+
+let initialize () = v := []
+
+let newref x =
+  begin
+    incr n;
+    v := x :: !v;
+    n
+  end
+
+let deref r = List.nth !v (!n - r)
+
+let setref r x =
+  let rec f m = function
+    | [] -> failwith ""
+    | y::ys ->
+        if m = 0 then x :: ys
+        else y :: (f (m - 1) ys)
+  in
+  v := f (!n - r) !v
