13d7f488cfd0d52ecaf205206063fedb536802e0
zehnpaard <zehnpaard@googlemail.com>
2019-06-23 23:24:18 +0900

    Add Read-Eval-Print

--- by-lang/inferred/bin/ex.ml
+++ by-lang/inferred/bin/ex.ml
@@ -3,9 +3,16 @@ open Inferredlang
 let check_eval exp =
   ignore (Typecheck.f exp); Eval.f exp
 
-let _ =
-  Lexing.from_channel stdin
+let f s =
+  Lexing.from_string s
   |> Parser.f Lexer.f
   |> check_eval
   |> Val.to_str
   |> print_endline
+
+let rec rep acc =
+  let s = read_line () in
+  if s = "" then f acc
+  else rep (acc ^ "\n" ^ s)
+
+let _ = (print_string ">>> "; rep "")
