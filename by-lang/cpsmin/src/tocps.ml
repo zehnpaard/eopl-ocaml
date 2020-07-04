73724c82f36e7fca0ca982f2c11080d17c7da5d7
zehnpaard <zehnpaard@googlemail.com>
2020-07-04 08:03:50 -0700

    Update call cps and add if tests

--- by-lang/cpsmin/src/tocps.ml
+++ by-lang/cpsmin/src/tocps.ml
@@ -5,17 +5,25 @@ let gensym =
   let f () = incr n; "gensym" ^ (string_of_int !n) in
   f 
 
+let is_simple = function
+| Const _ | Var _ -> true
+| _ -> false
+
 let rec g k e = match e with
 | Const _ | Var _ -> Call(k,[e])
 | Call(proc,es) ->
-  let proc_var = gensym () in
-  let vs = List.map (fun _ -> gensym ()) es in
-  let cont_body = Call(Var(proc_var), (List.map (fun v -> Var(v)) vs) @ [k]) in
-  let evs = List.rev @@ List.combine es vs in
+  let es = proc::es in
+  let es' = List.filter (fun e -> not @@ is_simple e) es in
+  let vs' = List.map (fun _ -> gensym ()) es' in
+  let evs = List.rev @@ List.combine es' vs' in
+  let f e = match List.assoc_opt e evs with
+  | None -> e
+  | Some v -> Var(v)
+  in
+  let es = List.map f es in
+  let cont_body = Call(List.hd es,List.tl es @ [k]) in
   let g' cont_body (e, v) = g (Proc([v],cont_body)) e in
-  let cont_body = List.fold_left g' cont_body evs in
-  let c = Proc([proc_var],cont_body) in
-  g c proc
+  List.fold_left g' cont_body evs
 | If(cond,yes,no) ->
   let v = gensym () in
   let cont = Proc([v],If(Var(v),g k yes,g k no)) in
